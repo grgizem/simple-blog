78990e8eea03a9aa6f6bf20ea7e8ccfd4692d775
Gizem Gür <grgizem@gmail.com>
2012-06-21 13:37:19 +0300

    registration fixed

--- mysite/blog/user_view.py
+++ mysite/blog/user_view.py
@@ -4,12 +4,11 @@ from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.views import password_reset
 
-
 from django.contrib.auth.decorators import login_required
 
 from django.template import RequestContext
 from django.contrib.auth.forms import UserCreationForm
-from django import oldforms as forms
+from django import forms
 
 from django.shortcuts import render_to_response, redirect
 
@@ -26,17 +25,15 @@ def logout(request):
 
 # new user registeration:
 def register(request):
-    form = UserCreationForm()
     if request.method == 'POST':
-        data = request.POST.copy()
-        errors = form.get_validation_errors(data)
-        if not errors:
+	form = UserCreationForm(request.POST)
+	if form.is_valid():
             new_user = form.save(data)
             return HttpResponseRedirect()
     else:
-        data, errors = {}, {}
+        form = UserCreatonForm()
     return render_to_response("register.html",
-            {'form':forms.FormWrapper(form,data,errors)})
+            {'form':forms})
 
 # reset password:
 def resetpass(request, template_name):
