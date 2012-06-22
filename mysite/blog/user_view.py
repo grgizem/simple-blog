3f4504c8b6c9cc806f9bd2f7e5d69136111aa1dc
Gizem Gür <grgizem@gmail.com>
2012-06-22 14:37:11 +0300

    unused codes deleted

--- mysite/blog/user_view.py
+++ mysite/blog/user_view.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponseRedirect
 
 from django.contrib.auth import logout as auth_logout
-from django.contrib.auth import login as auth_login
 from django.contrib.auth.views import password_reset
 
 from django.contrib.auth.decorators import login_required
@@ -10,11 +9,6 @@ from django.template import RequestContext
 from blog.forms import NewUserCreationForm
 
 from django.shortcuts import render_to_response, redirect
-
-# user login:
-def login(request):
-    auth_login(request)
-    return HttpResponseRedirect("/")
 
 # user logout:
 @login_required
