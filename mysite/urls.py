cdf2ee2e2e5a92dfec8f863c225d1b47192e3c0a
Gizem Gür <grgizem@gmail.com>
2012-06-22 13:50:19 +0300

    profile page added to url

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from blog.user_view import login, logout, register, resetpass
-from blog.views import newpost
+from blog.views import newpost, profile
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 admin.autodiscover()
@@ -14,9 +14,12 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
     url(r'^accounts/login/$',  'django.contrib.auth.views.login', {'template_name' : 'login.html'}),
     url(r'^accounts/logout/$', logout),
     url(r'^accounts/register/$', register),
+    url(r'^accounts/profile/$', profile),
+
     url(r'^newpost/$', newpost, name='newpost'),
 
 )
