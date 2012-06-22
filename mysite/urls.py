5992ef27ac49174ed301bc3f7c89f485014d647c
Gizem Gür <grgizem@gmail.com>
2012-06-22 09:29:22 +0300

    login url added

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,7 +13,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/login/$',  login),
+    url(r'^accounts/login/$',  'django.contrib.auth.views.login', {template_name : 'blog/login.html'}),
     url(r'^accounts/logout/$', logout),
     url(r'^accounts/register/$', register),
 
