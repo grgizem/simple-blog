90206d516ff4f4dddccf4cbb47b63971de994732
Gizem Gür <grgizem@gmail.com>
2012-06-25 10:07:46 +0300

    approve processes urls added

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,6 +23,11 @@ urlpatterns = patterns('',
     url(r'^password_change/done/$', 'django.contrib.auth.views.password_change_done', {'template_name' : 'profile.html'}),
     url(r'^accounts/changeemail/$', changeemail, name='changeemail'),
     url(r'^accounts/profile/$', profile),
+
+    url(r'^approve/$', approve),
+    url(r'^approve/entry/(?P<entry_id>\d+)/', approve_entry, name='approve_entry'),
+    url(r'^disapprove/entry/(?P<entry_id>\d+)/', disapprove_entry, name='dissapprove_entry'),
+
     url(r'^newpost/$', newpost, name='newpost'),
     
 )
