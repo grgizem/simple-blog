d40305045dc19e7e6c6eaf0eb7b302205f457874
Gizem Gür <grgizem@gmail.com>
2012-06-26 10:58:58 +0300

    approve operation added

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,4 +1,13 @@
 from blog.models import Entry
 from django.contrib import admin
 
-admin.site.register(Entry)
+def approve(modeladmin, request, queryset):
+	queryset.update(approvement=True)
+approve.short_description = "Approve selected entries"
+
+class EntryAdmin(admin.ModelAdmin):
+	list_display = ['title','author','approvement']
+	ordering = ['title']
+	actions = [approve]
+
+admin.site.register(Entry,EntryAdmin)
