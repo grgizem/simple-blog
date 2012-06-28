fb9dc309ee9c1be81438f4ff3afb74efacf26e70
Gizem Gür <grgizem@gmail.com>
2012-06-28 14:08:14 +0300

    user creation changed according to userprofile model

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -13,6 +13,18 @@ class NewUserCreationForm(UserCreationForm):
 
 	def save(self):
         	user = User.objects.create_user(self.cleaned_data["username"],self.cleaned_data["email"],self.cleaned_data["password1"])
+		user.is_active()=False
+		user.save()
+		# Activation properties
+		salt = sha.new(str(random.random())).hexdigest()[:5]
+		activation_key = sha.new(salt+user.username).hexdigest()
+		key_expires = datetime.datetime.today()+datetime.timedelta(2)
+		new_user = UserProfile(user=user, activation_key=activation_key, key_expires=key_expires)
+		new_user.save()
+		# sending an activation email
+		email_subject = 'Your new account confirmation'
+		email_body = 'Hello, %s, and thanks for registering for the blog \n To active your account, click following link within 48 hours: \n http://localhost:8000/accounts/confirm/%s' %(user.username, new_profile.activation_key)
+		send_email(email_subject, email_body, 'blog@blog.com', [user.email])
 
 class EntryForm(ModelForm):
 	class Meta:
