448c761f56b56265d70c71b07f35091450797420
Gizem Gür <grgizem@gmail.com>
2012-06-28 14:57:44 +0300

    spelling error corrected

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -28,5 +28,5 @@ class Entry(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
-    activation_key = models.CharField(maxlength=40)
+    activation_key = models.CharField(max_length=40)
     key_expires = models.DateTimeField()
